10d4859f17fc9da7e790bc6237de44b622d95ae0
RJ Nay <rjnay1984@gmail.com>
2020-09-24 13:50:06 -0500

    photo album function and test

--- photo_album.py
+++ photo_album.py
@@ -1,9 +1,21 @@
 import fire
+import requests
 
 
-def hello(name):
-    return 'Hello {name}!'.format(name=name)
+def request_album(id):
+    apiUrl = f'https://jsonplaceholder.typicode.com/photos?albumId={id}'
+    resp = requests.get(apiUrl)
+    photos = []
+    if resp.status_code != 200:
+        # This means something went wrong.
+        raise Exception(f'GET /photos {resp.status_code}')
+
+    # Print the id and title of each photo
+    for photo in resp.json():
+        photos.append(f"[{photo['id']}] {photo['title']}")
+
+    return photos
 
 
 if __name__ == '__main__':
-    fire.Fire()
+    fire.Fire(request_album)
--- tests.py
+++ tests.py
@@ -1,2 +1,12 @@
+from photo_album import request_album
+
+"""
+Test the length of the album, since it's
+consistent in the placeholder API.
+"""
+
+
 def test_photo_album():
-    pass
+    album_request = request_album(4)
+    print(album_request)
+    assert len(album_request) == 50
